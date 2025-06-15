91b493edcb70dde247818daaaf752ae6de6a8367
Sunil Varma <sunilvarma175@gmail.com>
2025-06-14 22:27:34 -0500

    updated portfolio commit

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-   base: '/SunilVarmaNandyala.github.io/',
+  base: '/SunilVarmaNandyala.github.io/', // this must match your repo name
+  plugins: [react()],
 });
